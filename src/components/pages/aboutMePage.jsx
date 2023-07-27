b08c19200ef60c46bb01f3bdb2f3eb95f361ab10
KyleM021199 <kylem021199@gmail.com>
2023-07-27 12:34:05 -0500

    completed About page

--- src/components/pages/aboutMePage.jsx
+++ src/components/pages/aboutMePage.jsx
@@ -1,15 +1,25 @@
 export default function About() {
     return (
       <div>
-        <h2>About Me</h2>
+         <h2>About Me</h2>
+        <div class="part-1">
+      <section class="card">
+        
+        <img id="Grad" src="./src/assets/images/Grad-Picture.jpg" alt = "Graduation photo"/>
         <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        I am a graduate of the University of Wisconsin-Whitewater looking to get a 
+          first step into an IT career. The idea of working with computers has always interested me all my life. 
+          When I learned of the existence of coding, it was extremely cool 
+          to me that lines of code can turn something static into an 
+          interactive marvel. At the same time, a bland and colorless page 
+          can become an interesting and colorful page that is easy to 
+          navigate with a combination of coding languages like 
+          JavaScript, CSS, and HTML. With my degree in Computer Science and 
+          love of learning about this field, I believe I will be a great 
+          addition to a software development crew or technical support team.
         </p>
+        </section>
+        </div>
       </div>
     );
   }
